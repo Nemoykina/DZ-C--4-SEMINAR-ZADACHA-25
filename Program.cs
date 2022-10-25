d456e20c3f339d4bd4d863bdcb241521022768c0
kate <katenik9391@gmail.com>
2022-10-25 13:46:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход двав числа (А и В)
+// и возводит число А в натуральную степень В.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+int GetPow(int A, int B)
+{
+    int pow = 1;
+    int i = 0;
+
+    while (i < B)
+    {
+        pow = pow * A;
+        i = i + 1;
+    }
+    return pow;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPow (y, x));
